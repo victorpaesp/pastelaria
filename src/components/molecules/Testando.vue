f410654394fad0f3c9f6493b5d7b44eea7db44d8
Victor Paes Pó <victorpaesp@gmail.com>
2022-02-24 18:08:31 -0300

    firebase teste

--- src/components/molecules/Testando.vue
+++ src/components/molecules/Testando.vue
@@ -1,58 +1,14 @@
 <template>
-  <div>
-    <el-popover
-      placement="bottom"
-      title="New Employee"
-      width="200"
-      trigger="click"
-    >
-      <el-input
+  <div class="testando">
+      <input
         placeholder="John Doe"
         v-model="name"
         @blur="createEmployee(name, date)"
-      ></el-input>
-      <el-button round slot="reference" type="success"
-        >Add New Employee</el-button
-      >
-    </el-popover>
-    <el-table
-      :data="
-        employeesData.filter(
-          (data) =>
-            !search || data.name.toLowerCase().includes(search.toLowerCase())
-        )
-      "
-      style="width: 100%;"
-    >
-      <el-table-column label="Date" prop="date"> </el-table-column>
-      <el-table-column label="Name" prop="name"> </el-table-column>
-      <el-table-column align="right">
-        <template slot="header" :slot-scope="scope">
-          <el-input v-model="search" size="mini" placeholder="Type to search" />
-        </template>
-        <template slot-scope="scope">
-          <el-popover
-            placement="bottom"
-            title="Edit Employee"
-            width="200"
-            trigger="click"
-          >
-            <el-input
-              placeholder="John Doe"
-              v-model="scope.row.name"
-              @blur="updateEmployee(scope.row.id, scope.row.name, date)"
-            ></el-input>
-            <el-button size="mini" slot="reference">Edit</el-button>
-          </el-popover>
-          <el-button
-            size="mini"
-            type="danger"
-            @click="deleteEmployee(scope.row.id)"
-            >Delete</el-button
-          >
-        </template>
-      </el-table-column>
-    </el-table>
+      />
+      <button round slot="reference" type="success"
+        >Add New Employee</button      >
+    
+        
   </div>
 </template>
 
@@ -139,5 +95,10 @@
 
 
 <style>
-
+    .testando {
+      z-index: 10;
+      background-color: red;
+      width: 500px;
+      height: 500px;
+    }
 </style>
